d8c03fe983151604b620f5fe592c27cc1edda0ba
Munenez <ronmunene2@gmail.com>
2025-03-20 10:13:28 +0300

    Adding new data

--- stkpush.php
+++ stkpush.php
@@ -40,7 +40,7 @@ $data_string = json_encode($curl_post_data);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-echo $curl_response = curl_exec($curl);
+$curl_response = curl_exec($curl);
 //ECHO  RESPONSE
 $data = json_decode($curl_response);
 $CheckoutRequestID = $data->CheckoutRequestID;
